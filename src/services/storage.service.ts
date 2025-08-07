85005b0fcb40418918460e26e442bf4edd982f4d
fhenouille <fhenouille@gmail.com>
2025-08-07 15:25:32 +0200

    add subscribe function

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,5 +1,25 @@
+const subscribers: Record<string, Set<(value: string | null) => void>> = {};
+
 const setItem = (key: string, value: string) => {
     localStorage.setItem(key, value);
+    if (subscribers[key]) {
+        subscribers[key].forEach((setter) => setter(value));
+    }
 }
 
-export const customStorage = { setItem };
+const getItem = (key: string) => {
+    return localStorage.getItem(key);
+}
+
+const subscribe = (keyToObserve: string, subscriberSetter: (value: string | null) => void) => {
+    if (!subscribers[keyToObserve]) {
+        subscribers[keyToObserve] = new Set();
+    }
+    subscribers[keyToObserve].add(subscriberSetter);
+
+    subscriberSetter(getItem(keyToObserve));
+
+    return () => { subscribers[keyToObserve].delete(subscriberSetter); };
+}
+
+export const customStorage = { setItem, subscribe };
